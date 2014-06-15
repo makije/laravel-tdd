3d17092152c7f13743f1e89921ac4a1d297ea3b1
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-06-15 20:41:06 +0200

    Rename the file

--- app/tests/MultiplicationTest.php
+++ app/tests/MultiplicationTest.php
@@ -4,16 +4,16 @@ class MultiplicationTest extends TestCase {
 
       public function testFindsTheProductOfPositiveNumbers()
       {
-            $addition = new Multiplication();
-            $product = $addition->run(5, 5);
+            $multiplication = new Multiplication();
+            $product = $multiplication->run(5, 5);
 
             $this->assertEquals(25, $product);
       }
 
       public function testFindsTheProductOfNegativeNumbers()
       {
-            $addition = new Multiplication();
-            $product = $addition->run(-5, -5);
+            $multiplication = new Multiplication();
+            $product = $multiplication->run(5, -5);
 
             $this->assertEquals(-25, $product);
       }
