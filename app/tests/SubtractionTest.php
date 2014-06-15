ebb68defe84a98279d8eb2f1e6c4bae8c7dccccf
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-06-15 20:31:55 +0200

    Fix the copy/paste error

--- app/tests/SubtractionTest.php
+++ app/tests/SubtractionTest.php
@@ -4,16 +4,16 @@ class SubtractionTest extends TestCase {
 
       public function testFindsTheDifferenceOfPositiveNumbers()
       {
-            $addition = new Subtraction();
-            $difference = $addition->run(5, 0);
+            $subtraction = new Subtraction();
+            $difference = $subtraction->run(5, 0);
 
             $this->assertEquals(-5, $difference);
       }
 
       public function testFindsTheDifferenceOfNegativeNumbers()
       {
-            $addition = new Subtraction();
-            $difference = $addition->run(-5, 0);
+            $subtraction = new Subtraction();
+            $difference = $subtraction->run(-5, 0);
 
             $this->assertEquals(5, $difference);
       }
